1bf7241c9ade111001cc1f035cc22b12fd2b1e1a
dam <m.maeda@newbees.jp>
2022-12-17 23:57:11 +0900

    feat:環境変数の指定方法を見直し

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,5 @@
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseClient = createClient(
-	import.meta.env.PUBLIC_SUPABASE_URL!,
-	import.meta.env.PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 export default supabaseClient;
